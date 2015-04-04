7ce605ee290f0b82ff5adf5465fb89ce4c5f1b9c
Nack <qwee6333@gmail.com>
2015-04-04 19:23:40 +0800

    first 1

---
+++ app/src/main/java/com/example/nack/homeworkiv/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.nack.homeworkiv;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+    private EditText edheight;
+    private EditText edweight;
+    private Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+    }
+    protected void findViews(){
+        edheight=(EditText)findViewById(R.id.edheight);
+        edweight=(EditText)findViewById(R.id.edweight);
+        btn=(Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(MainActivity.this,ResultActivity.class);
+                Bundle bundle=new Bundle();
+
+                double height =Double.parseDouble(edheight.getText().toString());
+                double weight =Double.parseDouble(edweight.getText().toString());
+
+                bundle.putDouble("height", height);
+                bundle.putDouble("weight", weight);
+                intent.putExtras(bundle);
+                startActivity(intent);
+
+
+
+
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
